017fe6adb663d1fa452a97d7799f11f2641ac7e5
Pedro Jara <pedrojara2405@gmail.com>
2018-10-14 00:33:00 -0500

    Adding MyLibraryTest

---
+++ MyLibraryTests/LevenshteinDistanceTests.cs
@@ -0,0 +1,26 @@
+﻿    using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLibrary;
+
+namespace MyLibraryTests
+{
+    [TestClass]
+    public class LevenshteinDistanceTests
+    {
+        [TestMethod]
+        public void XamarinAdnCamarin_ShouldReturn1()
+        {
+            //Arrange
+            var cadena1 = "xamarin";
+            var cadena2 = "camarin";
+            int ExpectedResult = 1;
+            LevenshteinDistance calculator = new LevenshteinDistance();
+
+            //Act
+            var result = calculator.Compute(cadena1, cadena2);
+
+            //Assert
+            Assert.AreEqual(ExpectedResult, result);
+        }
+    }
+}
